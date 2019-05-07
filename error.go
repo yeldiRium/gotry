401e4e1d93c1f8c991fd362933840c69627a10db
Hannes Leutloff <leutloff@marvelous.systems>
2019-05-07 22:38:35 +0200

    [TASK] remove obsolete ErrFIsMissing

--- error.go
+++ error.go
@@ -9,8 +9,6 @@ var (
 	// ErrMaxTriesReached is used when the retrying process does not succeed
 	// before MaxTries is reached.
 	ErrMaxTriesReached = errors.New("max tries reached")
-	// ErrFIsMissing is used when the parameter f to Try is nil.
-	ErrFIsMissing = errors.New("function f must be provided")
 )
 
 // IsTimeout returns true if the cause of the given error is a ErrTimeout.
@@ -21,9 +19,4 @@ func IsTimeout(err error) bool {
 // IsMaxTriesReached returns true if the cause of the given error is a ErrMaxTriesReached.
 func IsMaxTriesReached(err error) bool {
 	return err == ErrMaxTriesReached
-}
-
-// IsFIsMissing returns true if the cause of the given error is a ErrFIsMissing.
-func IsFIsMissing(err error) bool {
-	return err == ErrFIsMissing
 }
--- error_test.go
+++ error_test.go
@@ -13,9 +13,3 @@ func TestIsMaxTriesReached(t *testing.T) {
 		t.Error("ErrMaxTriesReached is somehow broken.")
 	}
 }
-
-func TestIsFIsMissing(t *testing.T) {
-	if !IsFIsMissing(ErrFIsMissing) {
-		t.Error("ErrFIsMissing is somehow broken.")
-	}
-}
